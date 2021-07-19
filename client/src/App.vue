d8ffd57901ec20f09983dd828bb282a0f6d966e0
Guru VII <guruvii@gmail.com>
2021-07-19 23:31:45 +0200

    saving progress

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav"></div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
+body {
+  background-color: #efefef;
+  font-size: 16px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  font-size: 16px;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  background: #fff;
+  background-color: rgb(255, 255, 255);
+  margin: 0;
+  border-radius: 4px;
+  overflow: hidden;
+  margin: 20px;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #2c3e50;
+  box-sizing: border-box;
+  box-shadow:rgba(0, 0, 0, 0.2) 0px 1px 3px 0px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 2px 1px -1px;
 }
 </style>
--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -7,13 +7,51 @@
       </tr>
       <tr v-for="item in data" :key="item.id" @click="openModal(item)">
         <td>
-          {{ item.title }}
+          {{ item.userId }}
         </td>
         <td>
-          {{ item.userId }}
+          {{ item.title }}
         </td>
       </tr>
     </table>
+    <div class="pagination">
+      <div>
+        <label>Stran:</label>
+        <select v-model="page" @change="getDataAndSetPagination()">
+          <option v-for="page in pagesArray" v-bind:value="page" :key="page">
+            {{ page + 1 }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <label>Limit:</label>
+        <select v-model="pageLimit" @change="changePageLimit()">
+          <option>10</option>
+          <option>20</option>
+          <option>50</option>
+        </select>
+      </div>
+      <div>
+        {{ page * pageLimit + 1 }} - {{ page * pageLimit + pageLimit }} out of
+        {{ count }}
+      </div>
+      <div>
+        <button @click="changePage(false)" :disabled="page === 0">⇽</button>
+        <button
+          @click="changePage(true)"
+          :disabled="page === this.pagesArray.slice(-1)[0]"
+        >
+          ⇾
+        </button>
+      </div>
+      <div>
+        <label>Sort:</label>
+        <select v-model="sort" @change="changePageLimit()">
+          <option value="1">ASC</option>
+          <option value="-1">DESC</option>
+        </select>
+      </div>
+    </div>
     <Details
       v-if="showDetails"
       @close="showDetails = false"
@@ -32,6 +70,10 @@ export default {
     return {
       data: {},
       count: 0,
+      page: 0,
+      pageLimit: 10,
+      sort: 1,
+      pagesArray: [],
       showDetails: false,
       selectedData: {
         body: "",
@@ -42,7 +84,6 @@ export default {
   },
   methods: {
     openModal: function ({ body, title, userId }) {
-      console.log("THIS RAN");
       this.selectedData = {
         body,
         title,
@@ -50,16 +91,81 @@ export default {
       };
       this.showDetails = true;
     },
+    getDataAndSetPagination: async function () {
+      const { data, count } = await this.getData(
+        this.page,
+        this.pageLimit,
+        this.sort
+      );
+      this.data = data;
+      this.count = count;
+      const maxPages = Math.ceil(count / this.pageLimit);
+      this.pagesArray = [];
+      for (let i = 0; i < maxPages; i++) {
+        this.pagesArray.push(i);
+      }
+    },
+    changePageLimit: async function () {
+      this.page = 0;
+      await this.getDataAndSetPagination();
+    },
+    changePage: async function (forward) {
+      console.log(this.pagesArray.slice(-1)[0]);
+      if (forward && this.page < this.pagesArray.slice(-1)[0]) {
+        this.page = this.page + 1;
+      } else if (this.page > 0) {
+        this.page = this.page - 1;
+      }
+      await this.getDataAndSetPagination();
+    },
   },
   mixins: [getData],
   components: { Details },
   async mounted() {
-    const { data, count } = await this.getData();
-    this.data = data;
-    this.count = count;
+    await this.getDataAndSetPagination();
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+table {
+  width: 50rem;
+  border-spacing: 0;
+  tr {
+    th {
+      font-size: 0.8rem;
+      font-weight: 600;
+      text-align: left;
+    }
+    td {
+      border-top: 1px rgba(0, 0, 0, 0.12) solid;
+      padding: 0.5rem 0.75rem;
+    }
+  }
+}
+.pagination {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  border-top: 1px rgba(0, 0, 0, 0.12) solid;
+  padding: 0.5rem 0.75rem;
+  div {
+    margin-right: 1rem;
+    font-size: 0.8rem;
+    label {
+      margin-right: 0.5rem;
+    }
+    button {
+      background-color: white;
+      border: none;
+      cursor: pointer;
+      &:hover {
+        background-color: #efefef;
+        border-radius: 4px;
+        font: 1rem;
+      }
+    }
+  }
+}
+</style>
